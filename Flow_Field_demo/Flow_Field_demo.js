3b3f5db9173bb6d0b24a412b787c790f179e5f99
Robus Gauli <robusgauli@gmail.com>
2016-12-26 21:51:55 +0545

    ASd

---
+++ Flow_Field_demo/Flow_Field_demo.js
@@ -0,0 +1,61 @@
+var step;
+var cols;
+var rows;
+
+//globa zoff
+var zoff = 0;
+
+var particles = [];
+
+var vectors;
+
+
+function setup() {
+  createCanvas(400, 400, P2D);
+  
+  step = 20;
+  cols = width/step;
+  rows = height/step;
+  for (var i=0; i< 200; i++){
+      pos = createVector(random(width), random(height));
+      particles.push(new Particle(pos));
+  }
+  vectors = new Array(rows * cols);
+  
+}
+
+function draw() {
+  
+  background(255);
+  var yoff = 0;
+  zoff += 0.005;
+  for (var y = 0; y < rows; y++){
+    var xoff = 0;
+    for (var x = 0; x < cols; x++){
+       xoff += 0.1;
+       var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+       var v = p5.Vector.fromAngle(angle);
+       v.mult(1)
+       // find the index
+       index = x + y * cols;
+       vectors[index] = v
+       //push();
+       //translate(x * step, y * step);
+       //rotate(v.heading());
+       //stroke(0)
+       //line(0, 0, step, 0)
+       //pop();
+    }
+    yoff += 0.1;
+  
+  }
+  
+  for (var j = 0; j < particles.length; j++){
+      
+      particles[j].show();
+      particles[j].update();
+      particles[j].edge();
+      particles[j].follow(vectors);
+      particles[j].applyForce(mouseX * 0.07, mouseY * 0.07);
+  }
+}
